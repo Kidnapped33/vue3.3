68f0931744a83b582176f193daddbf547cb08baf
ashine33 <ashine33@foxmail.com>
2023-05-22 17:43:11 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { User } from './types';
+
+// defineProps<User>()
+
+const count = ref(0)
+</script>
+
+<template>
+   <!-- <div>{{ $props?.userName }}</div> -->
+   <!-- <div>{{ $props?.userAge }}</div> -->
+  {{ count }}
+</template>
+
+<style scoped>
+</style>
